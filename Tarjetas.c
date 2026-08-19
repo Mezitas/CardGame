cff8ba8985657b4753286db485004862b1099250
mezita <diegopaz04717@gmail.com>
2026-08-19 12:28:17 -0500

    Se arreglaron unos errores, pero faltan funcionalidades

--- Tarjetas.c
+++ Tarjetas.c
@@ -14,27 +14,28 @@
  * @param Puntero a la matriz de enteros (Tarjetas) de 4x4
  * @return Void
  */
-void rellenarMatriz(int (*matriz)[4]);
+void rellenarMatriz(int *matriz);
 void mostrarMatriz(int matriz[16]);
 int main (int argc, char *argv[]){
 	int matriz[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-	rellenarMatriz(matriz);
+	rellenarMatriz(&matriz[0]);
+	mostrarMatriz(matriz);
 }
-void rellenarMatriz (int *matriz[16]){
+void rellenarMatriz (int *matriz){
 	srand(time(NULL));
 	int n = 1;
 	for(int i = 0; i < 16; i++){
 		n += rand() % 8;
-		matriz[i] = n;
+		*(matriz + i) = n;
+		n=1;
 	}
-	mostrarMatriz(matriz);
 }
 void mostrarMatriz(int matriz[16]){
 	printf("=====MATRIZ=====\n");
 	for(int i = 0; i < 4; i++){
 		for(int j = 0; j < 4; j++){
 			printf("|");
-			printf("%d", matriz[i*j]);
+			printf("%d", matriz[i*4+j]);
 		}
 		printf("\n");
 		printf("----------------\n");
