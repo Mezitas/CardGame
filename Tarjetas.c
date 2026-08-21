b9a692aa0a958b449e9c3ad23fcad6e350c27269
Juan Meza <diegopaz04717@gmail.com>
2026-08-21 09:47:31 -0500

    Algunos pequeños cambios xd

--- Tarjetas.c
+++ Tarjetas.c
@@ -22,8 +22,12 @@ void aleatorizarVector(int vector[16]);
 void empezarJuego();
 void mostrarOcultarMatriz(int matriz[4][4]);
 void mostrarMatrizOculta();
+void organizarVector(int vector[16]);
+void mostrarEntcontrado(int matriz[4][4], int x[16], int y[16]);
 int coordenadaY();
 int coordenadaX();
+int verificarX();
+int verificarY();
 void itzMenu1();
 void itzMenu2();
 
@@ -68,6 +72,8 @@ void aleatorizarVector(int vector[16]){
 
 void empezarJuego(){
 	int matriz[4][4] = {0};
+	int vectorPosicionesX[16] = {-1};
+	int vectorPosicionesY[16] = {-1};
 	int intentos = 0;
 	rellenarMatriz(matriz);
 	itzMenu1();
@@ -87,6 +93,21 @@ void mostrarMatrizOculta(){
 		for(int j = 0; j < 4; j++){
 			printf("|");
 			printf("*");
+		}
+		printf("\n");
+		printf("----------------\n");
+	}
+}
+void mostrarEntcontrado(int matriz[4][4], int x[16], int y[16]){
+	printf("=====MATRIZ=====\n");
+	for(int i = 0; i < 4; i++){
+		for(int j = 0; j < 4; j++){
+			printf("|");
+			if(i == x[i*4 + j] && j == y[i*4 + j]){
+				printf("%d", matriz[i][j]);
+			}else{
+				printf("*");
+			}
 		}
 		printf("\n");
 		printf("----------------\n");
@@ -115,4 +136,36 @@ int coordenadaX(){
 	printf("Ingrese la fila de la tarjeta: ");
 	scanf("%d", &coordenadaX);
 	return coordenadaX;
+}
+int verificarX(){
+	int x = coordenadaX();
+	if(x < 0 || x > 3){
+		printf("Coordenada invalida, ingrese un numero entre 0 y 3\n");
+		verificarX();
+	}
+	return x;
+}
+int verificarY(){
+	int y = coordenadaY();
+	if(y < 0 || y > 3){
+		printf("Coordenada invalida, ingrese un numero entre 0 y 3\n");
+		verificarY();
+	}
+	return y;
+}
+void organizarVector(int vector[16]){
+	int temp = 0;
+	for(int i = 0; i < 16; i++){
+		for(int j = 0; j < 16; j++){
+			if(vector[i] < vector[j]){
+				temp = vector[i];
+				vector[i] = vector[j];
+				vector[j] = temp;
+			}
+		}
+	}
+}
+void moverPosiciones(int x[16], int y[16], int posX, int posY, int index){
+	x[index] = posX;
+	y[index] = posY;
 }
